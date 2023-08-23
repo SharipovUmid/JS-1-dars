3cdcd0cf59b3f7f458c1dc483fec244b3ef512c6
ArtikbaevaBarno <142983980+SharipovUmid@users.noreply.github.com>
2023-08-23 17:57:55 +0500

    JS 1-dars
    26-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,79 @@
+/*console.log(8*3)*/
+/*alert("Saytimizga xush kelibsiz!")*/
+/*document.write("Men body oynasida ko'rinaman!")*/
+
+/*let x = 2;*/
+/*let y = 5;*/
+/*x = 10*/
+/*let x = 2, y = 5, z = 12*/
+/*let salom = "Assalomu alaykum!"*/
+/*let rang = "#f1f1f1"*/
+/*document.write(rang)*/
+
+/* Data types in JS */
+/*let son = 45;*/            /* number */
+/*let matn = "Dasturlash"*/    /* string */
+/*document.write(typeof matn)*/
+
+/* Matn in JS */
+/*let x = 5;*/
+/*x+=5;*/   /* x = 5 + 5 */
+/*x-=5;*/   /* x = 5 - 5 */
+/*x*=5;*/   /* x = 5 * 5 */
+/*x/=5;*/   /* x = 5 / 5 */
+/*let qoldiq = x%3;*/
+/*let natija = x**5*/       /* 5*5*5*5*5 */
+/*++x;*/
+/*--x;*/
+/*x++;*/
+/*x--;*/
+/*document.write(x+"matn")*/
+
+/* if (agar) */
+/* else (u holda) */
+/* else if (yoki bo'lmasa) => shart formulalari*/
+
+/*if (45 == 45){
+    console.log("Ular teng!")
+}
+else {
+    console.log("Ular teng emas!")
+}*/
+
+/*if (45 == 44){
+    console.log("Qiymatlari teng! - 1")
+}
+else if (45 == 46) {
+    console.log("Qiymatlari teng! - 2")
+}
+else {
+    console.log("Birorta qiymat teng emas!")
+}*/
+
+let a = 10;
+let b = 40;
+let c = 46;
+let d = 50;
+
+if (a == 10 && b == 40 && c == 46 && d == 50){
+    console.log("Qiymatlarning barchasi togri")
+}
+else {
+    console.log("Qiymatrlarning kamida bittasi notog'ri")
+}
+
+if (a == 10 || b == 40 || c == 46 || d == 50){
+    console.log("Qiymatrlarning kamida bittasi tog'ri")
+}
+else {
+    console.log("Qiymatlarning barchasi notog'ri")
+}
+
+
+
+
+
+
+
+
+
